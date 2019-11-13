83f0ca326976e38b21a276b0c4d8e85e337cfb69
Adeline Braun <adeline.braun@digipen.edu>
2019-11-13 09:05:02 -0800

    Switched to top down Added particle effect

--- Pong desinger/Assets/Scripts/PaddleMovement.cs
+++ Pong desinger/Assets/Scripts/PaddleMovement.cs
@@ -6,10 +6,10 @@ public class PaddleMovement : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D rb;
-    public KeyCode upKey;
-    public KeyCode downKey;
-    public float topWall;
-    public float bottomWall;
+    public KeyCode rightKey;
+    public KeyCode leftKey;
+    public float rightWall;
+    public float leftWall;
 
     // Start is called before the first frame update
     void Start()
@@ -21,22 +21,22 @@ public class PaddleMovement : MonoBehaviour
     void Update()
     {
         //if any change happens with paddle input
-        if (Input.GetKeyDown(upKey) || Input.GetKeyUp(upKey) || Input.GetKeyDown(downKey) || Input.GetKeyUp(downKey))
+        if (Input.GetKeyDown(rightKey) || Input.GetKeyUp(rightKey) || Input.GetKeyDown(leftKey) || Input.GetKeyUp(leftKey))
         {
             //if both keys are down, dont move paddle
-            if (Input.GetKey(upKey) && Input.GetKey(downKey))
+            if (Input.GetKey(rightKey) && Input.GetKey(leftKey))
             {
                 rb.velocity = new Vector2(0, 0);
             }
             //if only up key is down, move the paddle up
-            else if (Input.GetKey(upKey))
+            else if (Input.GetKey(rightKey))
             {
-                rb.velocity = new Vector2(0, speed);
+                rb.velocity = new Vector2(speed, 0);
             }
             //if only down key is down, move paddle down
-            else if (Input.GetKey(downKey))
+            else if (Input.GetKey(leftKey))
             {
-                rb.velocity = new Vector2(0, -speed);
+                rb.velocity = new Vector2(-speed, 0);
             }
             //if neither key is down, dont move paddle
             else
@@ -45,12 +45,12 @@ public class PaddleMovement : MonoBehaviour
             }
         }
         //if paddle is at the bottom wall, stop it from going farther down
-        if(rb.position.y < bottomWall && Input.GetKey(downKey))
+        if(rb.position.x < leftWall && Input.GetKey(leftKey))
         {
             rb.velocity = new Vector2(0, 0);
         }
         //if paddle is at the top wall, stop it from going farther up
-        if(rb.position.y > topWall && Input.GetKey(upKey))
+        if(rb.position.x > rightWall && Input.GetKey(rightKey))
         {
             rb.velocity = new Vector2(0, 0);
         }
---
+++ Pong desinger/Assets/Scripts/ParticlesOnCollide.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticlesOnCollide : MonoBehaviour
+{
+    public GameObject FireworksAll;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("Player"))
+        {
+            Explode();
+        }
+    }
+    void Explode()
+    {
+        GameObject firework = Instantiate(FireworksAll, transform.position, Quaternion.identity);
+        firework.GetComponent<ParticleSystem>().Play();
+    }
+}
